f745a68db5c0f2641fc124126f95668b8fbebe5e
Rahul Shrestha <rahul.shrestha@selu.edu>
2016-03-30 19:34:41 -0500

    skeleton

---
+++ webservice_test/webservice_testTests/webservice_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  webservice_testTests.swift
+//  webservice_testTests
+//
+//  Created by Rahul Shrestha on 3/30/16.
+//  Copyright © 2016 SLU. All rights reserved.
+//
+
+import XCTest
+@testable import webservice_test
+
+class webservice_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
